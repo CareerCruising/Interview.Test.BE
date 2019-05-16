f0acdc66a9e8652fb1d61e280d2192b74c53a198
ruthramya <ruthramya.n@hotmail.com>
2019-05-16 19:05:48 -0400

    Add files via upload

---
+++ GraduationTrackerTests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GraduationTracker.Tests.Unit
+{
+    [TestClass]
+    public class GraduationTrackerTests
+    {
+        #region TestData
+       
+         // Reusing the test data for both of test cases          
+        
+        GraduationTracker tracker = new GraduationTracker();
+        //Get the testdata from Repository class
+        Diploma diploma = Repository.GetDiploma(1);
+
+        Student[] studentTestData = new[]
+        {
+                Repository.GetStudent(1),
+                Repository.GetStudent(2),
+                Repository.GetStudent(3),
+                Repository.GetStudent(4)
+            };
+        #endregion
+        public List<Tuple<bool, STANDING>> GetStudentsData()
+        {
+            var graduated = new List<Tuple<bool, STANDING>>();
+
+            foreach (var student in studentTestData)
+            {
+                graduated.Add(tracker.HasGraduated(diploma, student));
+            }
+            return graduated;
+        }
+                 
+        [TestMethod]
+        /// <summary>
+        /// Test case to know if any student has no credits or failed.
+        /// Now the test has only single  responsible to check for GraduationFailed Student
+        /// </summary>  
+        public void GraduationFailedTests()
+        {
+
+            var graduated = GetStudentsData();
+            Assert.IsFalse(graduated.Any(x => x.Item2 == STANDING.None));
+            Assert.IsTrue(graduated.Any(x => x.Item1 == false));
+            Assert.IsTrue(graduated.Any(x => x.Item2 == STANDING.Remedial));
+        }
+
+      
+        [TestMethod]
+        /// <summary>
+        /// Method:HasCreditsTest
+        /// Test case to test  if any students has credits.
+        /// </summary>
+        public void HasCreditsTests()
+        {
+            var graduated = GetStudentsData();
+            Assert.IsTrue(graduated.Any());
+            Assert.IsTrue(graduated.Count == 4);
+            Assert.IsTrue(graduated[1].Item2 == STANDING.MagnaCumLaude);
+        
+        }
+
+    }
+}
---
+++ RepositoryTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace GraduationTracker.Tests.Unit
+{
+    [TestClass]
+    public class RepositoryTests
+    {
+        [TestMethod]
+        public void IsStudentValidTest()
+        {
+            var student = Repository.GetStudent(2);
+            Assert.IsTrue(student.Id == 2);
+            Assert.IsTrue(student.Courses != null);
+            Assert.IsTrue(student.Courses.Any(m => m.Mark >= 0 && m.Mark<=100));
+
+
+        }
+
+        [TestMethod]
+        public void IsDiplomaValidTest()
+        {
+            var diploma = Repository.GetDiploma(1);
+            Assert.IsTrue(diploma != null);
+            Assert.IsFalse(diploma.Id == 2);        
+            Assert.IsTrue(diploma.Credits >= 0);
+            Assert.IsTrue(diploma.Requirements.Contains(102));
+           
+
+        }
+        [TestMethod]
+        public void IsRequirementValidTest()
+        {
+            var requirement = Repository.GetRequirement(102);
+            Assert.IsTrue(requirement != null);
+            Assert.IsTrue(requirement.MinimumMark>49);
+            Assert.IsTrue(requirement.Credits==1);
+
+        }
+    }
+}
