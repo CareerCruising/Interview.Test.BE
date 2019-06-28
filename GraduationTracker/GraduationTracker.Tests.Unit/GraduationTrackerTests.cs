90116bdd9c9911d8e1b990f6c46ed1f4cfa2012f
Luan Caius <luancaius@outlook.com>
2019-06-27 21:35:06 -0400

    feat: added unit test, created mock data, improved coverage.

--- GraduationTracker/GraduationTracker.Tests.Unit/GraduationTrackerTests.cs
+++ GraduationTracker/GraduationTracker.Tests.Unit/GraduationTrackerTests.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -9,80 +9,155 @@ namespace GraduationTracker.Tests.Unit
     public class GraduationTrackerTests
     {
         [TestMethod]
-        public void TestHasCredits()
+        public void HasGraduated_ThreeStudentsPassesOneFail_ThreeTrueOneFalse()
         {
-            var tracker = new GraduationTracker();
+            // Arrange
+            GraduationTracker tracker = new GraduationTracker();
 
-            var diploma = new Diploma
-            {
-                Id = 1,
-                Credits = 4,
-                Requirements = new int[] { 100, 102, 103, 104 }
-            };
+            Diploma diploma = Repository.GetDiploma(1);
 
-            var students = new[]
+            List<Student> students = GetStudents();
+
+            List<Tuple<bool, STANDING>> graduated = new List<Tuple<bool, STANDING>>();
+
+            // Act
+            foreach (Student student in students)
             {
-               new Student
-               {
-                   Id = 1,
-                   Courses = new Course[]
-                   {
-                        new Course{Id = 1, Name = "Math", Mark=95 },
-                        new Course{Id = 2, Name = "Science", Mark=95 },
-                        new Course{Id = 3, Name = "Literature", Mark=95 },
-                        new Course{Id = 4, Name = "Physichal Education", Mark=95 }
-                   }
-               },
-               new Student
-               {
-                   Id = 2,
-                   Courses = new Course[]
-                   {
-                        new Course{Id = 1, Name = "Math", Mark=80 },
-                        new Course{Id = 2, Name = "Science", Mark=80 },
-                        new Course{Id = 3, Name = "Literature", Mark=80 },
-                        new Course{Id = 4, Name = "Physichal Education", Mark=80 }
-                   }
-               },
-            new Student
-            {
-                Id = 3,
-                Courses = new Course[]
-                {
-                    new Course{Id = 1, Name = "Math", Mark=50 },
-                    new Course{Id = 2, Name = "Science", Mark=50 },
-                    new Course{Id = 3, Name = "Literature", Mark=50 },
-                    new Course{Id = 4, Name = "Physichal Education", Mark=50 }
-                }
-            },
-            new Student
-            {
-                Id = 4,
-                Courses = new Course[]
-                {
-                    new Course{Id = 1, Name = "Math", Mark=40 },
-                    new Course{Id = 2, Name = "Science", Mark=40 },
-                    new Course{Id = 3, Name = "Literature", Mark=40 },
-                    new Course{Id = 4, Name = "Physichal Education", Mark=40 }
-                }
+                graduated.Add(tracker.HasGraduated(diploma, student));
             }
 
-
-            //tracker.HasGraduated()
-        };
-            
-            var graduated = new List<Tuple<bool, STANDING>>();
-
-            foreach(var student in students)
-            {
-                graduated.Add(tracker.HasGraduated(diploma, student));      
-            }
-
-            
-            Assert.IsFalse(graduated.Any());
-
+            // Assert
+            int totalPass = graduated.Count(a => a.Item2 != STANDING.Remedial);
+            int totalFail = graduated.Count(a => a.Item2 == STANDING.Remedial);
+            Assert.AreEqual(3, totalPass);
+            Assert.AreEqual(1, totalFail);
         }
 
+        [TestMethod]
+        public void HasGraduated_AllStudentsFail_AllFalse()
+        {
+            // Arrange
+            GraduationTracker tracker = new GraduationTracker();
 
+            Diploma diploma = Repository.GetDiploma(1);
+
+            List<Student> students = MockGetStudents(new[] { 49, 30, 20, 0 });
+
+            List<Tuple<bool, STANDING>> graduated = new List<Tuple<bool, STANDING>>();
+
+            // Act
+            foreach (Student student in students)
+            {
+                graduated.Add(tracker.HasGraduated(diploma, student));
+            }
+
+            // Assert
+            int totalFail = graduated.Count(a => a.Item2 == STANDING.Remedial);
+            Assert.AreEqual(students.Count, totalFail);
+        }
+
+        [TestMethod]
+        public void HasGraduated_AllStudentsPass_AllTrue()
+        {
+            // Arrange
+            GraduationTracker tracker = new GraduationTracker();
+
+            Diploma diploma = Repository.GetDiploma(1);
+
+            List<Student> students = MockGetStudents(new[] { 100, 90, 80, 50 });
+
+            List<Tuple<bool, STANDING>> graduated = new List<Tuple<bool, STANDING>>();
+
+            // Act
+            foreach (Student student in students)
+            {
+                graduated.Add(tracker.HasGraduated(diploma, student));
+            }
+
+            // Assert
+            int totalPass = graduated.Count(a => a.Item2 != STANDING.Remedial);
+            Assert.AreEqual(students.Count, totalPass);
+        }
+
+        [TestMethod]
+        public void Student_GraduatedWithMagnaCumLaude_HasSameStanding()
+        {
+            // Arrange
+            GraduationTracker tracker = new GraduationTracker();
+
+            Diploma diploma = Repository.GetDiploma(1);
+
+            Student student = MockGetStudents(new[] { 100 }).First();
+
+            List<Tuple<bool, STANDING>> graduated = new List<Tuple<bool, STANDING>>();
+
+            // Act
+            var result = tracker.HasGraduated(diploma, student);
+            student.Standing = result.Item2;
+
+            // Assert
+            Assert.AreEqual(STANDING.MagnaCumLaude, student.Standing);
+        }
+
+        [TestMethod]
+        public void Diploma_SumCreditsRequirement_IsEqualToDiploma()
+        {
+            // Arrange
+            Diploma diploma = Repository.GetDiploma(1);
+
+            // Act
+            var sumRequirementsCredits = 0;
+            for (int i = 0; i < diploma.Requirements.Length; i++)
+            {
+                sumRequirementsCredits += Repository.GetRequirement(diploma.Requirements[i]).Credits;
+            }
+
+            // Assert
+            Assert.AreEqual(diploma.Credits, sumRequirementsCredits);
+        }
+
+        #region Mock Methods
+
+        private List<Student> GetStudents()
+        {
+            List<Student> list = new List<Student>
+            {
+                Repository.GetStudent(1),
+                Repository.GetStudent(2),
+                Repository.GetStudent(3),
+                Repository.GetStudent(4)
+            };
+
+            return list;
+        }
+
+        private List<Student> MockGetStudents(int[] marks)
+        {
+            List<Student> listStudents = new List<Student>();
+
+            for (int i = 0; i < marks.Length; i++)
+            {
+                Student student = new Student
+                {
+                    Id = i + 1,
+                    Courses = MockGetCourses(marks[i])
+                };
+                listStudents.Add(student);
+            }
+
+            return listStudents;
+        }
+
+        private Course[] MockGetCourses(int mark)
+        {
+            return new[]
+            {
+                new Course {Id = 1, Name = "Math", Mark = mark},
+                new Course {Id = 2, Name = "Science", Mark = mark},
+                new Course {Id = 3, Name = "Literature", Mark = mark},
+                new Course {Id = 4, Name = "Physichal Education", Mark = mark}
+            };
+        }
+        #endregion
     }
 }
