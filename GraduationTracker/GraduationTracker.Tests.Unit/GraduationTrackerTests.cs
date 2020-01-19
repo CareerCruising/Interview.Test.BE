7dbd1bcc4db67d9e004d2de53733f1508dd0ed11
Nereu Oliveira <nereu@np-c.io>
2020-01-19 15:41:54 -0500

    fix: Adjust graduation testcase
    - Rename unit test to be coherent with method
    - Add array of expected results to be asserted against

--- GraduationTracker/GraduationTracker.Tests.Unit/GraduationTrackerTests.cs
+++ GraduationTracker/GraduationTracker.Tests.Unit/GraduationTrackerTests.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace GraduationTracker.Tests.Unit
 {
@@ -9,7 +6,7 @@ namespace GraduationTracker.Tests.Unit
     public class GraduationTrackerTests
     {
         [TestMethod]
-        public void TestHasCredits()
+        public void TestHasGraduated()
         {
             var tracker = new GraduationTracker();
 
@@ -17,72 +14,65 @@ namespace GraduationTracker.Tests.Unit
             {
                 Id = 1,
                 Credits = 4,
+
                 Requirements = new int[] { 100, 102, 103, 104 }
             };
 
+            var expect = new bool[] { true, true, true, false };
+
             var students = new[]
             {
-               new Student
-               {
-                   Id = 1,
-                   Courses = new Course[]
-                   {
+                new Student
+                {
+                    Id = 1,
+                    Courses = new Course[]
+                    {
                         new Course{Id = 1, Name = "Math", Mark=95 },
                         new Course{Id = 2, Name = "Science", Mark=95 },
                         new Course{Id = 3, Name = "Literature", Mark=95 },
                         new Course{Id = 4, Name = "Physichal Education", Mark=95 }
-                   }
-               },
-               new Student
-               {
-                   Id = 2,
-                   Courses = new Course[]
-                   {
+                    }
+                },
+                new Student
+                {
+                    Id = 2,
+                    Courses = new Course[]
+                    {
                         new Course{Id = 1, Name = "Math", Mark=80 },
                         new Course{Id = 2, Name = "Science", Mark=80 },
                         new Course{Id = 3, Name = "Literature", Mark=80 },
                         new Course{Id = 4, Name = "Physichal Education", Mark=80 }
-                   }
-               },
-            new Student
-            {
-                Id = 3,
-                Courses = new Course[]
+                    }
+                },
+                new Student
                 {
-                    new Course{Id = 1, Name = "Math", Mark=50 },
-                    new Course{Id = 2, Name = "Science", Mark=50 },
-                    new Course{Id = 3, Name = "Literature", Mark=50 },
-                    new Course{Id = 4, Name = "Physichal Education", Mark=50 }
-                }
-            },
-            new Student
-            {
-                Id = 4,
-                Courses = new Course[]
+                    Id = 3,
+                    Courses = new Course[]
+                    {
+                        new Course{Id = 1, Name = "Math", Mark=50 },
+                        new Course{Id = 2, Name = "Science", Mark=50 },
+                        new Course{Id = 3, Name = "Literature", Mark=50 },
+                        new Course{Id = 4, Name = "Physichal Education", Mark=50 }
+                    }
+                },
+                new Student
                 {
-                    new Course{Id = 1, Name = "Math", Mark=40 },
-                    new Course{Id = 2, Name = "Science", Mark=40 },
-                    new Course{Id = 3, Name = "Literature", Mark=40 },
-                    new Course{Id = 4, Name = "Physichal Education", Mark=40 }
+                    Id = 4,
+                    Courses = new Course[]
+                    {
+                        new Course{Id = 1, Name = "Math", Mark=40 },
+                        new Course{Id = 2, Name = "Science", Mark=40 },
+                        new Course{Id = 3, Name = "Literature", Mark=40 },
+                        new Course{Id = 4, Name = "Physichal Education", Mark=40 }
+                    }
                 }
-            }
+            };
 
-
-            //tracker.HasGraduated()
-        };
-            
-            var graduated = new List<Tuple<bool, STANDING>>();
-
-            foreach(var student in students)
+            for (var i = 0; i < students.Length; i++)
             {
-                graduated.Add(tracker.HasGraduated(diploma, student));      
+                var graduated = tracker.HasGraduated(diploma, students[i]);
+                Assert.AreEqual(expect[i], graduated.Item1);
             }
-
-            
-            Assert.IsFalse(graduated.Any());
-
         }
-
-
     }
 }
