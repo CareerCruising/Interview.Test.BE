40c8798347e99d4c6f1cafc049b6ad7732261619
yukonfan <yukonfan@outlook.com>
2020-01-22 16:00:19 -0500

    commit the changes in the unit test

--- GraduationTracker/GraduationTracker.Tests.Unit/GraduationTrackerTests.cs
+++ GraduationTracker/GraduationTracker.Tests.Unit/GraduationTrackerTests.cs
@@ -22,61 +22,11 @@ namespace GraduationTracker.Tests.Unit
 
             var students = new[]
             {
-                new Student
-                {
-                    Id = 1,
-                    Courses = new Course[]
-                    {
-                        new Course{Id = 1, Name = "Math", Mark = 95 },
-                        new Course{Id = 2, Name = "Science", Mark = 95 },
-                        new Course{Id = 3, Name = "Literature", Mark = 95 },
-                        new Course{Id = 4, Name = "Physichal Education", Mark = 95 }
-                    }
-                },
-                new Student
-                {
-                    Id = 2,
-                    Courses = new Course[]
-                    {
-                        new Course{Id = 1, Name = "Math", Mark = 80 },
-                        new Course{Id = 2, Name = "Science", Mark = 80 },
-                        new Course{Id = 3, Name = "Literature", Mark = 80 },
-                        new Course{Id = 4, Name = "Physichal Education", Mark = 80 }
-                    }
-                },
-                new Student
-                {
-                    Id = 3,
-                    Courses = new Course[]
-                    {
-                        new Course{Id = 1, Name = "Math", Mark = 50 },
-                        new Course{Id = 2, Name = "Science", Mark = 50 },
-                        new Course{Id = 3, Name = "Literature", Mark = 50 },
-                        new Course{Id = 4, Name = "Physichal Education", Mark = 50 }
-                    }
-                },
-                new Student
-                {
-                    Id = 4,
-                    Courses = new Course[]
-                    {
-                        new Course{Id = 1, Name = "Math", Mark = 40 },
-                        new Course{Id = 2, Name = "Science", Mark = 40 },
-                        new Course{Id = 3, Name = "Literature", Mark = 40 },
-                        new Course{Id = 4, Name = "Physichal Education", Mark = 40 }
-                    }
-                },
-                new Student
-                {
-                    Id = 5,
-                    Courses = new Course[]
-                    {
-                        new Course{Id = 1, Name = "Math", Mark = 95 },
-                        new Course{Id = 2, Name = "Science", Mark = 95 },
-                        new Course{Id = 3, Name = "Literature", Mark = 95 },
-                        new Course{Id = 4, Name = "Physichal Education", Mark = 40 }
-                    }
-                }
+               new Student { Id = 1 },
+               new Student { Id = 2 },
+               new Student { Id = 3 },
+               new Student { Id = 4 },
+               new Student { Id = 5 }
             };
 
             var graduated = new List<Tuple<bool, STANDING>>();
