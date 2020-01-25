c287ad12431d9215e6572f3cdcb0916eaf442a50
Jainam Shah <shahjainam45@yahoo.com>
2020-01-24 21:04:18 -0500

    Restructured files in the solution
    -added models into model namespace
    -added repository into repository namespace

--- GraduationTracker/GraduationTracker/Repositories/Repository.cs
+++ GraduationTracker/GraduationTracker/Repositories/Repository.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using GraduationTracker.Models;
 
-namespace GraduationTracker
+namespace GraduationTracker.Repositories
 {
     public class Repository
     {
