28cbfc7cc0bbe47b2ac6358c586ebc1adc5dda02
Logan Kim <logan.kim0212@gmail.com>
2021-01-30 20:53:45 -0500

    fixed to access interface instead of a class

--- GraduationTracker/GraduationTracker/GraduationTracker.cs
+++ GraduationTracker/GraduationTracker/GraduationTracker.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GraduationTracker.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +9,7 @@ namespace GraduationTracker
 {
     public partial class GraduationTracker
     {
-        private readonly Repository repository = new Repository();
+        private readonly IRepository repository = new Repository();
 
         public Student GetStudentById(int id)
         {
